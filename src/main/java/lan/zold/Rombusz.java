1a6ed87506b64b201b0382fd3ea529183d373ad7
pwfszerko <bendeguzszeri@gmail.com>
2024-01-10 10:51:24 +0100

    teszt1

---
+++ src/main/java/lan/zold/Rombusz.java
@@ -0,0 +1,7 @@
+package lan.zold;
+
+public class Rombusz {
+    public double calcArea(double side){
+        return 0.0;
+    }
+}
---
+++ src/test/java/RombuszTest.java
@@ -0,0 +1,16 @@
+import java.util.InputMismatchException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import lan.zold.Rombusz;
+public class RombuszTest {
+    Rombusz romb;
+    @BeforeTest
+    public void setUp(){
+        this.romb = new Rombusz();
+    }
+    
+    
+}
